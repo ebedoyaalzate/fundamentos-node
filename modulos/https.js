eb6dbec7b1627ccfa9f697cc39dfc4803f1c7bd3
ebedoyaalzate <esteban.bedoya.contractor@bbva.com>
2023-11-29 16:28:53 -0500

    add https example

---
+++ modulos/https.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+http.createServer(router).listen(3000);
+
+console.log('Escuchando http en el puerto 3000');
+
+function router(req, res) {
+  console.log('nueva petición');
+  console.log(req.url);
+
+  switch (req.url) {
+    case '/hola':
+      res.write('hola, que tal');
+      res.end();
+      break;
+
+    default:
+      res.write('Error 404: No se lo que quieres');
+      res.end();
+      break;
+  }
+
+  // res.writeHead(201, {'Content-Type': 'text/plain'});
+  // res.write('hola, ya se usar http de NodeJS');
+
+  // res.end();
+}
